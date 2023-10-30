f2617681e031e8e2399638275090391733de8828
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-10-30 17:24:41 +0200

    Аdded first task from exam

---
+++ Mid Exam/Foundation Retake Exam/01.theEntsMarch.js
@@ -0,0 +1,11 @@
+function theEntsMarch(n, m, h, t) {
+    let total = n + ((Math.floor(t / h) * m));
+
+    console.log(total);
+}
+
+theEntsMarch(50, 10, 2, 6);
+theEntsMarch(30, 5, 3, 9);
+theEntsMarch(40, 8, 1, 7);
+theEntsMarch(100, 20, 2, 10);
+theEntsMarch(60, 12, 3, 15);
