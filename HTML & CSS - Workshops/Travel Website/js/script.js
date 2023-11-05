4a932c17694f6c37380550308b1f1738d78b0af9
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-11-05 10:34:55 +0200

    Added hamburger menu functionality

---
+++ HTML & CSS - Workshops/Travel Website/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const overlay = document.querySelector('[data-overlay]');
+const navOpenBtn = document.querySelector('[data-nav-open-btn]');
+const navbar = document.querySelector('[data-navbar]');
+const navCloseBtn = document.querySelector('[data-nav-close-btn]');
+const navLinks = document.querySelectorAll('[data-nav-link]');
+
+const navElementArr = [navOpenBtn, navCloseBtn, overlay];
+
+const navToggleEvent = function (el) {
+    for (let i = 0; i < el.length; i++) {
+        el[i].addEventListener('click', function () {
+            navbar.classList.toggle("active");
+            overlay.classList.toggle("active");
+        });
+    }
+}
+
+navToggleEvent(navElementArr);
+navToggleEvent(navLinks);
