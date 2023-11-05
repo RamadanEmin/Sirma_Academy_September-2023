cd0d5e7a9b5c1c8c50a9ad670c686b11ae72a905
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-11-05 20:46:31 +0200

    Added 'Go to top' button and functionality

--- HTML & CSS - Workshops/Travel Website/js/script.js
+++ HTML & CSS - Workshops/Travel Website/js/script.js
@@ -19,3 +19,16 @@ const navToggleEvent = function (el) {
 
 navToggleEvent(navElementArr);
 navToggleEvent(navLinks);
+
+const header = document.querySelector('[data-header]');
+const goTopBtn = document.querySelector('[data-go-top]');
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY >= 200) {
+        header.classList.add("active");
+        goTopBtn.classList.add("active");
+    } else {
+        header.classList.remove("active");
+        goTopBtn.classList.remove("active");
+    }
+});
