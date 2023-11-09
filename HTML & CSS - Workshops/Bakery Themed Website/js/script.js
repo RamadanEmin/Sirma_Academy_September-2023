bfbbcd2892c72970819a028052699e4e8376f430
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-11-09 19:32:48 +0200

    Added mobile menu functionality

--- HTML & CSS - Workshops/Bakery Themed Website/js/script.js
+++ HTML & CSS - Workshops/Bakery Themed Website/js/script.js
@@ -4,12 +4,30 @@ const searchForm = document.querySelector('.search-form');
 const cartIcon = document.querySelector('.cart-icon');
 const cartItemsContainer = document.querySelector('.cart-items-container');
 
+const menuIcon = document.querySelector('.menu-icon');
+const navbar = document.querySelector('.navbar');
+
 searchIcon.addEventListener('click', () => {
     searchForm.classList.add('active');
     cartItemsContainer.classList.remove('active');
+    navbar.classList.remove('active');
 });
+
 
 cartIcon.addEventListener('click', () => {
     cartItemsContainer.classList.add('active');
     searchForm.classList.remove('active');
+    navbar.classList.remove('active');
 });
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.add('active');
+    cartItemsContainer.classList.remove('active');
+    searchForm.classList.remove('active');
+});
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    cartItemsContainer.classList.remove('active');
+    searchForm.classList.remove('active');
+};
