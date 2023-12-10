d6126fa556532be186e538aa713583e19d4f9f16
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-10 21:26:37 +0200

    Update Unsorted table component

--- React_basketball_statistics/src/components/templates/UnsortedTable.jsx
+++ React_basketball_statistics/src/components/templates/UnsortedTable.jsx
@@ -1,9 +1,19 @@
+import { useContext } from 'react';
+import { dataContext } from '../../contexts/dataContext';
+import Table from '../organisms/Table/Table';
+
 function UnsortedTable() {
+    const { data } = useContext(dataContext);
 
     return (
-        <div>
-            <h1>Unsorted Table</h1>
-        </div>
+        <>
+            {(data.length > 0) ? (
+                <Table
+                    columns={['Player name', 'Team', 'Time played(s)', 'Points scored']}
+                    data={data}
+                />
+            ) : null}
+        </>
     );
 }
 
