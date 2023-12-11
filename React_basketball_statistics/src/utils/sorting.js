4ae5c5b2c580fa034bbe13c30778a67a83ef800c
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-11 15:47:28 +0200

    Added sort player by playing time function

--- React_basketball_statistics/src/utils/sorting.js
+++ React_basketball_statistics/src/utils/sorting.js
@@ -29,7 +29,26 @@ function mostPointScoredPlayerSorting(data) {
     return sortedData;
 }
 
+function bestPlayerScoreSrotingByTime(data) {
+    let sortedData;
+    if (data.length > 0) {
+        sortedData = data.map(row => [...row]);
+
+        sortedData.map(row => {
+            const pointsPerSecond = Number(row[3]) / Number(row[2]);
+            row.push(pointsPerSecond.toFixed(3));
+
+            return row;
+        });
+
+        sortedData.sort((a, b) => b[4] - a[4]);
+    }
+
+    return sortedData;
+}
+
 export {
     playerPointScoreInOneGameSorting,
     mostPointScoredPlayerSorting,
+    bestPlayerScoreSrotingByTime,
 };
